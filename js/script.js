766cb6cf7177ff436ae3020ef86aedb3f4c05d93
lorenzotade <lorenzo.tade@gmail.com>
2021-04-21 15:47:35 +0200

    terminato

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(function(){
+  // creo un event listener sui div.drop-trigger per
+  // captare l'hover del mouse
+  $('.drop-trigger').mouseenter(function(){
+    // quando avviene, una funzione anonima cerca
+    // all'interno di tutti i .drop-trigger
+    // un tag che abbia classe .dropdown E .active
+    // contemporaneamente. Quando lo trova rimuove la
+    // classe chiudendo il dropdown
+    $('.drop-trigger').find('.dropdown.active').removeClass('active');
+    // a questo punto cerca se nel .drop-trigger in oggetto
+    // è presente un elemento classe .dropdown e ad esso
+    // aggiunge la classe .active aprendo il dropdown
+    $(this).find('.dropdown').addClass('active');
+  });
+
+  // creo un altro event listener sempre sui div.drop-trigger
+  // per captare questa volta il click del mouse
+  $('.drop-trigger').click(function(){
+    // quando avviene, la funzione anonima cerca
+    // all'interno dell'elemento in oggetto
+    // l'elemento con classe .dropdown e ad esso
+    // rimuove la classe active chiudendo il dropdown
+    $(this).find('.dropdown').removeClass('active');
+  });
+
+});
